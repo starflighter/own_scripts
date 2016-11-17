85145f4b0b1b2b2dce40936e153facd4760ffed2
starflighter <starflighter.one@t-online.de>
2016-11-17 22:21:03 +0100

    change licence info

--- iptables/iptables_aufraeumen.sh
+++ iptables/iptables_aufraeumen.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-## Copyright (c) Yvonne Fischer
+## Copyright 2016 (c) Yvonne Fischer
 ##
 ## This program is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License
-## as published by the Free Software Foundation; either version 2
-## of the License, or (at your option) any later version.
+## as published by the Free Software Foundation; either version 3
+## of the License, or any later version.
 ##
 ## This program is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
